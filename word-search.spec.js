5c875b46e4d83e79aa0e45573df8e60a6c1cde07
Matthew Morgan <matt@dulcetsoftware.com>
2017-09-27 18:19:48 -0400

    Actually make it a square

--- word-search.spec.js
+++ word-search.spec.js
@@ -4,11 +4,11 @@ describe('word search', ()=> {
 
   test('can get a square of letters', ()=> {
     const grid = [
-        "asquare",
-        "ofwords"
+        "cat",
+        "dog",
+        "sun"
     ];
 
     const w = new WordSearch(grid);
-
   })
 });
