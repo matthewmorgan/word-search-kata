ce4675799b7a8c0cfac2bbe0bac8f46e25361fd8
Matthew Morgan <matt@dulcetsoftware.com>
2017-09-27 18:18:07 -0400

    Pass an actual square of words to an actual new call

--- word-search.spec.js
+++ word-search.spec.js
@@ -1,7 +1,14 @@
 import WordSearch from './word-search';
 
 describe('word search', ()=> {
+
   test('can get a square of letters', ()=> {
+    const grid = [
+        "asquare",
+        "ofwords"
+    ];
+
+    const w = new WordSearch(grid);
 
   })
 });
