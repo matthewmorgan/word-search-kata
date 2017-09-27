89002eee9842c7ce41d7c9b9b6d4366f1585ea6b
Matthew Morgan <matt@dulcetsoftware.com>
2017-09-27 18:52:37 -0400

    Generalize to find words left to right

--- word-search.js
+++ word-search.js
@@ -1,46 +1,32 @@
 class WordSearch {
-  constructor(grid){
+  constructor(grid) {
     this.validateGridDimensions(grid);
     this.grid = grid;
   }
 
-  find(words){
-    if (words[0] === "cat"){
-      return {
-        cat: {
-          start: [1,1],
-          end: [1,3]
-        }
+  find(words) {
+    const word = words[0];
+    const result = {};
+
+    for (let i = 0; i < this.grid.length; i++) {
+      const row = this.grid[i];
+      if (row.indexOf(word) !== -1) {
+        const start = row.indexOf(word) + 1;
+        const end = start + word.length - 1;
+        result[word] = {
+          start: [i + 1, start],
+          end:   [i + 1, end]
+        };
+        return result;
       }
     }
-
-    if (words[0] === "dog"){
-      if (this.grid[1] === "bdog"){
-        return {
-          dog: {
-            start: [2,2],
-            end: [2,4]
-          }
-        }
-      } else {
-        return {
-          dog: {
-            start: [2,1],
-            end: [2,3]
-          }
-        }
-      }
-
-    }
-
-    return {
-      "bat": undefined
-    }
+    return result;
   }
-  validateGridDimensions(grid){
+
+  validateGridDimensions(grid) {
     const rowCount = grid.length;
     const invalidRowLengthCount = grid.filter(row => row.length !== rowCount).length;
-    if (invalidRowLengthCount){
+    if (invalidRowLengthCount) {
       throw new Error("Grid is not square.");
     }
   }
